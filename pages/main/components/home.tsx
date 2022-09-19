051838dbbe916d4da0ebc7bd0bb1393be2e88bfc
zhanglu <zhangluweiyi@gmail.com>
2022-09-19 18:07:25 +0800

    add support chain

--- pages/main/components/home.tsx
+++ pages/main/components/home.tsx
@@ -1,11 +1,16 @@
 import styled from "styled-components"
 import Image from "next/image"
-import footerPic from 'public/images/home/footer.png'
 import SocialMedia from "components/SocialMedia"
 import { forwardRef } from "react"
 import ScrollOverpack from "components/ScrollOverpack"
 
 import HomeImage from './homeImage'
+import footerPic from 'public/images/home/footer_bg.png'
+import polygon from 'public/images/home/polygon.png'
+import mina from 'public/images/home/mina.png'
+import eth from 'public/images/home/eth.png'
+import bnb from 'public/images/home/bnb.png'
+import useMatchBreakpoints from "hooks/useMatchBreakpoints"
 
 const HomeContainer = styled.div`
   position: relative;
@@ -45,15 +50,20 @@ const FooterContainer = styled(ImageContainer)`
   bottom: 30px;
   width: 100%;
   height: 40px;
+  padding-left: 220px;
   ${({ theme }) => theme.sm} {
     position: relative;
+    height: 14px;
+    padding-left: 50px;
     left: 0;
   }
 `
 
 const TextContainer = styled.div`
   position: absolute;
+  left: 0;
   bottom: 100px;
+  width: 100%;
   ${({ theme }) => theme.sm} {
     position: relative;
     bottom: 0px;
@@ -73,6 +83,9 @@ const Text = styled.span`
 `
 
 const Home = forwardRef((props, ref) => {
+
+  const { isMobile } = useMatchBreakpoints()
+
   return <>
     <HomeContainer ref={ref} id="home">
       <ScrollOverpack>
@@ -90,6 +103,10 @@ const Home = forwardRef((props, ref) => {
         </TextContainer>
         <FooterContainer>
           <Image layout="fill" objectFit="contain" quality={100} src={footerPic} />
+          <Image layout="intrinsic" objectFit="contain" height={isMobile ? 10 : 40} width={isMobile ? 40 : 120} quality={100} src={eth} />
+          <Image layout="intrinsic" objectFit="contain" height={isMobile ? 10 : 40} width={isMobile ? 40 : 120} quality={100} src={mina} />
+          <Image layout="intrinsic" objectFit="contain" height={isMobile ? 10 : 40} width={isMobile ? 40 : 120} quality={100} src={polygon} />
+          <Image layout="intrinsic" objectFit="contain" height={isMobile ? 10 : 40} width={isMobile ? 40 : 120} quality={100} src={bnb} />
         </FooterContainer>
       </ScrollOverpack>
     </HomeContainer>
