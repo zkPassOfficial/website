0c232baebc950c03fc7c70dee9f67877b8f723c3
stoneomo <zhangluweiyi@gmail.com>
2023-12-08 17:35:40 +0800

    change tina config

--- tina/config.js
+++ tina/config.js
@@ -3,17 +3,13 @@ import { homeCollection } from './content/pages/home'
 
 const branch = process.env.VERCEL_GIT_COMMIT_REF || 'main'
 
-const clientId = '9fd64f97-4de5-408e-9e8a-374b852a9b0e'
-const token = 'df98d0a7e72953f929078db391dc3703b06d4e53'
-const searchToken = '25830bc973dd308f4ec59c93aef1e79ad54efd61'
-
 export default defineConfig({
   branch,
-  clientId: clientId,
-  token: token,
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+  token: process.env.TINA_TOKEN,
   search: {
     tina: {
-      indexerToken: searchToken,
+      indexerToken: process.env.TINA_SEARCH_TOKEN,
       stopwordLanguages: ['eng'],
     },
     indexBatchSize: 300,
